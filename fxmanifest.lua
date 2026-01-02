b7e1467bd70141785ea49375919a1f1c5a9ec600
AZZAM DEVELOPMENT <115076514+AzzamCyber@users.noreply.github.com>
2026-01-02 15:47:06 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
-lua54 'yes' -- Aktifkan Lua 5.4
+lua54 'yes'
 
 author 'Acam / Azzam Codex'
 description 'Professional Boarding Pass Connecting UI'
 version '2.0.0'
 
--- Load Config dulu, baru Server Logic
+
 server_scripts {
     'config.lua',
     'server.lua'
